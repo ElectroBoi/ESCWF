e655af1a500a759c404c1658c5745e120c0dd0a7
ElectroBoi <64182319+ElectroBoi@users.noreply.github.com>
2025-04-11 03:32:54 +0700

    NewPing Library untuk membaca data sensor UltraSonic

---
+++ 2. Library_NewPing.ino
@@ -0,0 +1,31 @@
+#include <NewPing.h>
+
+// pin arduino yang terhubung dengan pin Trigger & Echo sensor ultrasonik
+#define TRIGGER_PIN  13 
+#define ECHO_PIN     12  
+
+// jarak maksimum sensor (di sesuaikan dengan spesifikasi sensor yang digunakan, distance maksimum sensor WallFollower Lab adalah 200cm atau 2 Meter)
+#define MAX_DISTANCE 200 
+
+// inisialisasi Library NewPing dengan setting diatas
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+
+void setup() {
+
+  //inisialisasi komunikasi Serial agar dapat debugging
+  Serial.begin(115200); 
+
+}
+
+void loop() {
+  //Menunggu 50ms setiap pembacaan sensor, waktu delay terendah untuk jarak 200cm 29ms
+  delay(50);                     
+
+  /*
+  Menulis hasil pembacaan sensor ke komunikasi Serial dengan memanggil object newping yang terinisialisasi diatas, 
+  lalu memanggil fungsi "ping_cm()" untuk membaca data yang telah dihitung menjadi cm
+  */
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm()); 
+  Serial.println("cm");
+}
