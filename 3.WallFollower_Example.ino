af2b9c0cd90d32c561c7e8ddc118481136c9fe18
ElectroBoi <64182319+ElectroBoi@users.noreply.github.com>
2025-04-11 07:20:58 +0700

    WallFollower Example Code

--- 3.WallFollower_Example.ino
+++ 3.WallFollower_Example.ino
@@ -15,8 +15,8 @@
 
 
 // pin arduino yang terhubung dengan pin Trigger & Echo sensor ultrasonik
-#define TRIGGER_PIN  4 
-#define ECHO_PIN     5  
+#define TRIGGER_PIN  5 
+#define ECHO_PIN     4  
 
 // jarak maksimum sensor (di sesuaikan dengan spesifikasi sensor yang digunakan, distance maksimum sensor WallFollower Lab adalah 200cm atau 2 Meter)
 #define MAX_DISTANCE 200 
